9195b745738b08ac77a77241622c5eef15873fb2
project <joaoalvaro700@gmail.com>
2024-08-24 13:37:16 -0300

    Convertendo Strings

--- SistemaVendas/Models/Seller.cs
+++ SistemaVendas/Models/Seller.cs
@@ -1,20 +1,35 @@
-﻿namespace SistemaVendas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVendas.Models
 {
     public class Seller
     {
 
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
         public string Email { get; set; }
+
+        [Display(Name = "Aniversário")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthData { get; set; }
+
+        [Display(Name = "Salário")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Salary { get; set; }
+
+
         public Departament Departament { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
         public int DepartamentId { get; set; }
 
         public Seller() { }
 
-        public Seller( string name, string email, DateTime birthData, double salary, Departament departament) {
+        public Seller(string name, string email, DateTime birthData, double salary, Departament departament)
+        {
             Name = name;
             Email = email;
             BirthData = birthData;
@@ -22,24 +37,28 @@
             Departament = departament;
         }
 
-        public Seller(int id, string name, string email, DateTime birthData, double salary, Departament departament) {
+        public Seller(int id, string name, string email, DateTime birthData, double salary, Departament departament)
+        {
             Id = id;
             Name = name;
             Email = email;
             BirthData = birthData;
             Salary = salary;
-            Departament = departament; 
+            Departament = departament;
         }
 
-        public void AddSales(SalesRecord sr) {
+        public void AddSales(SalesRecord sr)
+        {
             Sales.Add(sr);
         }
 
-        public void RemoveSales(SalesRecord sr) {
+        public void RemoveSales(SalesRecord sr)
+        {
             Sales.Remove(sr);
         }
 
-        public double TotalSales(DateTime initial, DateTime final) {
+        public double TotalSales(DateTime initial, DateTime final)
+        {
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
     }
--- SistemaVendas/Program.cs
+++ SistemaVendas/Program.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SistemaVendas.Data;
 using SistemaVendas.Services;
+using System.Globalization;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,6 +24,15 @@ builder.Services.AddScoped<DepartamenteService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+var enUS = new CultureInfo("en-US");
+var localizations = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+
+};
+app.UseRequestLocalization(localizations);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
